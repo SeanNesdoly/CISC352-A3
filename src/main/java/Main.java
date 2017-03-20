3315c218885ba79793626c2975cdc4b22568f8d6
srnesdoly <srnesdoly@me.com>
2017-03-20 08:04:55 -0400

    initial gradle build framework along with an updated .gitignore to handle netbeans files

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/*
+ * The main class for running the following 2 problems:
+ *      1. N-Queens
+ *      2. AlphaBeta Pruning
+ *
+ * CISC 352 Assignment 3
+ * Sean Nesdoly & Mary Hoekstra
+ * March 19th, 2017
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+    
+}
