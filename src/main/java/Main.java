47ea9c613c00635380287cfaf84af163c522abe1
srnesdoly <srnesdoly@me.com>
2017-03-20 17:23:35 -0400

    update build to select problem instance to run

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,16 @@
  */
 
 public class Main {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        int mode = (args.length > 0 ? Integer.parseInt(args[0]) : 0);
+        if (mode == 1) {
+            System.out.println("Running N-Queens\n============================");
+        } else if (mode == 2) {
+            System.out.println("Running AlphaBeta Pruning\n============================");
+        } else {
+            System.out.println("Invalid argument. Enter the desired problem number you would like to run (./gradlew -Pmode=n):\n\t1=N-Queens problem\n\t2=AlphaBeta Pruning");
+        }
     }
     
 }
